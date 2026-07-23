110520cc86a51a61ffc51ee1a5ec13f6b83f374e
Yeferson <duqueyeferson@gmail.com>
2026-07-23 18:07:46 -0500

    feat: agregar respuesta de movimientos financieros

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log("Hola, Yefer");
+const http = require("http");
+
+const server = http.createServer((request, response) => {
+  if (request.url === "/") {
+    response.setHeader("Content-Type", "application/json");
+
+    response.end(
+      JSON.stringify({
+        message: "Bienvenido a Finanzas App",
+        app: "finanzas-app",
+      }),
+    );
+  } else if (request.url === "/movements") {
+    const movements = [
+      {
+        description: "Mercado",
+        amount: 250000,
+        type: "expense",
+      },
+      {
+        description: "Salario",
+        amount: 2000000,
+        type: "income",
+      },
+    ];
+
+    response.setHeader("Content-Type", "application/json");
+
+    response.end(JSON.stringify(movements));
+  } else {
+    response.end("Ruta no encontrada");
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Servidor ejecutándose en http://localhost:3000");
+});
